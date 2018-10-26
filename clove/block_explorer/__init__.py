1f1653c212d6cb23fab97766f338dfb34852749c
Merlin <merlin.buczek@gmail.com>
2018-10-26 10:41:04 +0000

    added brekken explorer

--- clove/block_explorer/__init__.py
+++ clove/block_explorer/__init__.py
@@ -2,10 +2,12 @@ from .blockcypher import BlockcypherAPI
 from .cryptoid import CryptoidAPI
 from .etherscan import EtherscanAPI
 from .insight import InsightAPIv4
+from .graphql import GraphQL
 
 __all__ = (
     BlockcypherAPI,
     CryptoidAPI,
     EtherscanAPI,
     InsightAPIv4,
+    GraphQL,
 )
